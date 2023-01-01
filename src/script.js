41465161fa8607967ad9e552e72d3d51724374cd
sWallyx <mikelsmartinez@aol.com>
2023-01-01 12:31:18 +0100

    add new functionality

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
+let timerJob
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    setInterval(function () {
+    timerJob = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -15,37 +17,50 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-function calcular_tiempo_plataforma(original_timer){
-  // 1 minute and 40 seconds
-  time_to_discount = 60 + 40
-  resucitar = original_timer - time_to_discount
-  calculate_timer_time(resucitar)
-} 
+function calculatePlatformTime(original_timer) {
+    // 1 minute and 40 seconds
+    time_to_discount = 60 + 40
+    platform_revive_time = original_timer - time_to_discount
+    minutes, seconds = calculateTimerTime(platform_revive_time)
 
-function calculate_timer_time(tiempo){
-  minutes = parseInt(tiempo / 60, 10);
-  seconds = parseInt(tiempo % 60, 10);
+    document.querySelector('#platform_timer').textContent = minutes + ":" + seconds
+}
 
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+function calculateTimerTime(timestamp) {
+    minutes = parseInt(timestamp / 60, 10);
+    seconds = parseInt(timestamp % 60, 10);
 
-  document.querySelector('#platform_timer').textContent = minutes + ":" + seconds
- 
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    return minutes, seconds
 }
 
 function callMeteor() {
+    document.getElementById("revive_element").style.display = "block"; 
     timer = document.querySelector('#header').textContent
-    
+
     minutes = parseInt(timer.split(":")[0], 10)
     seconds = parseInt(timer.split(":")[1], 10)
     button_time = (minutes * 60) + seconds
 
-    calcular_tiempo_plataforma(button_time)
+    calculatePlatformTime(button_time)
+}
+
+function restartTimer() {
+    resetMeteorCounterStatus()
+    clearInterval(timerJob)
+    var fiveMinutes = 60 * 20
+    var display = document.querySelector('#header');
+    startTimer(fiveMinutes, display);
 }
 
 
+function resetMeteorCounterStatus(){
+    document.getElementById("revive_element").style.display = "none"; 
+    document.querySelector('#platform_timer').textContent = ""
+}
+
 window.onload = function () {
-    var fiveMinutes = 60 * 20,
-        display = document.querySelector('#header');
-    startTimer(fiveMinutes, display);
+    restartTimer()
 };
